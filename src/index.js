8dca83fee188f62ef0eb4bce7a86fc6501eb9747
rayhanalmim <rayhanalmim1@gmail.com>
2025-05-10 16:09:32 +0600

    Add note for legacy OpenSSL provider usage in index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+// set NODE_OPTIONS=--openssl-legacy-provider && npm start
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
